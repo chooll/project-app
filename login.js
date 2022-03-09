f834918ebd324e5ed61bdcc2c6f15ca79eb71cca
Anat <62662868+chooll@users.noreply.github.com>
2022-03-09 12:46:03 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+const login = document.getElementById("loginInput");
+
+const password = document.getElementById("passwordInput");
+
+function getAccout(login, password) {
+    return { "login": login, "password": password };
+}
+
+const accounts = [getAccout("gusev", "12345"),
+    getAccout("bogdanov", "12345"),
+    getAccout("boshina", "12345")
+];
+
+function loginInApp() {
+    let st = false;
+    for (let i = 0; i != accounts.length; i++) {
+        if (login.value == accounts[i]["login"]) {
+            if (password.value == accounts[i]["password"]) {
+                st = true;
+                window.location.href = 'index.html';
+                break;
+            }
+        }
+    }
+    if (!st) {
+        let error = document.getElementById("errorText")
+        error.innerHTML = `<p>* Неправильный логин или пароль</p>`;
+    }
+}
--- main.js
+++ main.js
@@ -1,59 +1,78 @@
-const heroes = document.getElementById("heroes");
-const f_skill = document.getElementById("first-skill");
-const s_skill = document.getElementById("second-skill");
-const t_skill = document.getElementById("three-skill");
-const ulta = document.getElementById("ulta");
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getStructDay(day, s1, s2, s3, s4, s5) {
+    // Parse schedule; 
+    return {
+        "name-day": day,
+        "list-subject": [s1, s2, s3, s4, s5]
+    };
 }
 
-function getRandomHeroes() {
-    let list = ["Storm Spirit", "Magnus", "Medusa", "Phoenix", "Alchemic", "Arc Warden", "Mirana", "Draw Ranger", "Ursa", "Ember Spirit", "Earth Spirit", "Marci", "Techies", "Templar Assasian", "Phantom Assasian", "Spirit Breaker", "Dawnbreaker", "Crystal Maiden", "Void Spirit", "Void", "Chen", "Pugna", "Pudge", "Sniper", "Terrorblade", "Clockwerk", "Jaggernaut", "Shadow Shaman", "Slardar", "Keep of Light", "Wraight King", "Shadow Fiend", "Windranger", "Axe", "Abbadon", "Zeus", "Witch Doctor", "Sven", "Riki", "Batrider", "Outword Devaider", "Sand King", "Qeen of Pain", "Bane", "Huscar", "Tiny", "Lone Druid", "Lucan", "Viper", "Spectre", "Bounty Hunter", "Lina", "Lich", "Monkey King", "Tinker", "Enchatress"];
-    return list[getRandomInt(list.length)]
+function generateSchedule() {
+    const schedule = document.getElementById("schedule-list");
+
+    const dayList = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+
+    const getDate = ((day, date) => {
+        return `<article class="day-with-subject">
+        <div class="date">
+            <h3 class="day">` + day + `</h3>
+            <p class="month">` + date + `</p>
+        </div>`;
+    });
+
+    const daySubjectList = ((subject, time) => {
+        return `            
+        <article class="block-subject">
+        <p class="time">` + time + `</p>
+        <p class="text">` + subject + `</p>
+        </article>`
+    });
+
+    for (let i = 0; i != 5; i++) {
+        schedule.innerHTML += getDate(dayList[i], "12 Февраля");
+        for (let j = 0; j != 4; j++) {
+            schedule.innerHTML += daySubjectList("Программирование в Excel", "10:20 12:00");
+        }
+        schedule.innerHTML += "</article>";
+    }
 }
 
-function emptyButtonFunction() {
-    main_panel = document.getElementById("main-panel");
-    main_panel.innerHTML += `
-        <div class="main-panel w-block"> 
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
-            <button>1</button>
+function getSubjectOnDay(day) {
+    const block = document.getElementById("container-schedule");
 
-        </div>
-    `;
+    block.innerHTML += `   
+    <div class="date">
+    <h3 class="day">` + day["name-day"] + `</h3>
+    <p class="month">12 февраля</p>
+    </div>`
+
+    for (let i = 0; i != day["list-subject"].length - 1; i++) {
+        block.innerHTML += `
+        <article class="block-subject">
+        <p class="time">10:00 12:00</p>
+        <p class="text">` + day["list-subject"][i] + `</p>
+        </article>`
+    }
+
+    block.innerHTML += `<button onclick="window.location.href = 'schedule.html'" class="full-order">
+    Посмотреть расписание
+    </button>`;
 
 }
 
-function generateHero() {
-    heroes.innerHTML = "";
-    f_skill.innerHTML = "";
-    s_skill.innerHTML = "";
-    t_skill.innerHTML = "";
-    ulta.innerHTML = "";
 
-    heroes.innerHTML = getRandomHeroes();
-    f_skill.innerHTML += getRandomHeroes();
-    s_skill.innerHTML += getRandomHeroes();
-    t_skill.innerHTML += getRandomHeroes();
-    ulta.innerHTML += getRandomHeroes();
+function generateNews(amountNews = 0) {
+    const newsBlock = document.getElementById("news-block");
+    const getNews = ((textNews) => {
+        return `
+        <article class="news-block">
+        <div class="image"></div>
+        <p class="text-news">` + textNews + `</p>
+        </article>`
+    });
+
+    for (let i = 0; i != 5; i++) {
+        newsBlock.innerHTML += getNews("В НовГУ прошел парад технологий студентов, который собрал больше 1 000 школьноков");
+    }
 }
 
-function clearInfoHero() {
-    heroes.innerHTML = "Hero";
-    f_skill.innerHTML = "1. Skill";
-    s_skill.innerHTML = "2. Skill";
-    t_skill.innerHTML = "3. Skill";
-    ulta.innerHTML = "Ulta";
-}
-
-const createHeroButton = document.getElementById("Button");
-createHeroButton.addEventListener("click", generateHero);
-
-const deleteHeroButton = document.getElementById("button-clear");
-deleteHeroButton.addEventListener("click", clearInfoHero)
-
-const emptyButton = document.getElementById("button-empty");
-emptyButton.addEventListener("click", emptyButtonFunction);
+getSubjectOnDay(getStructDay("Понедельник", "Программное решение для бизнеса", "Технология разработки и защиты базы данных", "Теория вероятности", "Теория вероятности"));
